80510c6ceb2a5dc9e3ff055a9f81ab737949ae36
Dhonatan da Silva <dhonatan_silva@hotmail.com>
2025-10-01 17:05:15 -0300

    Logs

---
+++ 0x04/log/LoggerInFile.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class LoggerInFile {
+
+	public static void main(String[] args) throws SecurityException, IOException {
+
+		Logger logger = Logger.getLogger("MyLog");
+
+		FileHandler fileHandler = new FileHandler("logs.txt", 100, 10);
+
+		logger.addHandler(fileHandler);
+
+		SimpleFormatter simpleFormatter = new SimpleFormatter();
+
+		fileHandler.setFormatter(simpleFormatter);
+
+		logger.info("teste de Log");
+
+		logger.info("Olá, eu sou o teste da classe principal");
+
+	}
+
+}
